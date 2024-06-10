407d50743bb9716ff8c81f464f4e67de04bd9ce9
x20surya <suryanshuc659@gmail.com>
2024-06-10 22:54:47 +0530

    add credentials.js

---
+++ credentials.js
@@ -0,0 +1,10 @@
+//Oauth 2 Credentials
+export const CLIENT_ID =
+  "";
+export const CLIENT_SECRET = "";
+export const REDIRECT_URI = [""];
+
+//DynamoDB Credentials
+export const ACCESS_KEY_ID = "";
+export const SECRET_ACCESS_KEY = "";
+export const TableName = "";
