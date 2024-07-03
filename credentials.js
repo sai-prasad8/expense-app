2229f4b09f605c69824ee08e42b454b8bada07d9
x20surya <77159015+x20surya@users.noreply.github.com>
2024-07-03 17:32:49 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -5,7 +5,7 @@ export const CLIENT_SECRET = "";
 export const REDIRECT_URI = [""];
 
 //DynamoDB Credentials
-export const ACCESS_KEY_ID = "AKIAU6GDXQBNRCQMV3LB";
+export const ACCESS_KEY_ID = "";
 export const SECRET_ACCESS_KEY = "";
 export const region = "";
 export const TableName = "";
