6e16d06824ab8fa30033c18a087b0981301a0e12
x20surya <77159015+x20surya@users.noreply.github.com>
2024-07-03 17:32:39 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,10 +1,11 @@
 //Oauth 2 Credentials
 export const CLIENT_ID =
-  "374822619059-indihhvqpv8ue6f17plme2f5cq6aju05.apps.googleusercontent.com";
-export const CLIENT_SECRET = "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";
-export const REDIRECT_URI = ["http://localhost:3000/logged"];
+  "";
+export const CLIENT_SECRET = "";
+export const REDIRECT_URI = [""];
 
 //DynamoDB Credentials
 export const ACCESS_KEY_ID = "AKIAU6GDXQBNRCQMV3LB";
-export const SECRET_ACCESS_KEY = "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";
-export const TableName = "emails";
+export const SECRET_ACCESS_KEY = "";
+export const region = "";
+export const TableName = "";
