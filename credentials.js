f0434548a64399283afa6332ce631fa8710a54c0
x20surya <suryanshuc659@gmail.com>
2024-07-04 16:41:17 +0530

    Update index.js

---
+++ credentials.js
@@ -0,0 +1,4 @@
+export const ACCESS_KEY_ID = "AKIAU6GDXQBNVYEDUDEG";
+export const SECRET_ACCESS_KEY = "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";
+export const region = "ap-south-1";
+export const TableName = "emails";
--- index.js
+++ index.js
@@ -14,4 +14,12 @@ async function getAllEmail(){
     console.log(response);
 }
 
-getAllEmail();
+app.get("/", async (req, res)=>{
+    const response = await documentClient.send(
+        new ScanCommand({
+            TableName : TableName,
+        })
+    )
+    const {Items} = response;
+    res.json({Items})
+})
